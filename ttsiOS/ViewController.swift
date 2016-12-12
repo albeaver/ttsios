6182c934912d8c1f27fd77ab2a5bc2feb2cca2eb
albeaver <albeaver@alexs-macbook-air.local>
2016-12-11 16:16:36 -0800

    Initial Commit

---
+++ ttsiOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ttsiOS
+//
+//  Created by Alexander Beaver on 12/11/16.
+//  Copyright © 2016 Alexander Beaver. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
