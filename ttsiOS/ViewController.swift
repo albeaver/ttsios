45e5d2aff7aac980f771c60fe3b0b86289a54d10
albeaver <albeaver@alexs-macbook-air.local>
2016-12-11 16:25:45 -0800

    I have a working app!

--- ttsiOS/ViewController.swift
+++ ttsiOS/ViewController.swift
@@ -7,14 +7,26 @@
 //
 
 import UIKit
-
+import AVFoundation
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var textField: UITextField!
+    func speak(toSpeak:String){
+        let synth = AVSpeechSynthesizer()
+        
+        let utterance = AVSpeechUtterance(string: toSpeak)
+        utterance.voice = AVSpeechSynthesisVoice(language: "en-AU")
+        utterance.rate = 0.5
+        synth.speak(utterance)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func onPush(_ sender: Any) {
+        speak(toSpeak: textField.text!)
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
